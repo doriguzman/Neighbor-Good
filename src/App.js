4773513e05e48880a484c61561b11925061545f5
newton-brooks <newtonbrooks.ac.c4q.nyc>
2018-03-03 15:46:18 -0500

    MVP

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Link, Route, Switch } from 'react-router-dom'
+import NeighborGood from './components/neighborGood'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <nav>
+          <Link to='/NeighborGood'>Home</Link>{" "}
+          <Link to='/NeighborGood/Map'>Map</Link>
+        </nav>
+        <Route path='/NeighborGood' component={NeighborGood}/>
       </div>
     );
   }
---
+++ src/components/neighborGood.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import axios from 'axios'
+import { Link, Route, Switch } from 'react-router-dom'
+import Map from './map'
+import ComplaintInfo from './complaintInfo'
+
+
+
+class NeighborGood extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            userSelects: '',
+            SelectedComplaint: null
+        }
+    }
+
+
+    handleComplaint = (complaint) => {
+        this.setState({
+            SelectedComplaint: complaint
+        })
+    }
+
+    handleMap = () => {
+        const { SelectedComplaint } = this.state
+        return (
+            <div>
+                <h1 className="title">311's Reported Complaints</h1>
+                {SelectedComplaint ? ComplaintInfo(SelectedComplaint) : <strong> Choose a complaint </strong>}
+                <Map onComplaintClick={this.handleComplaint} />
+                <br />
+            </div>
+        )
+    }
+
+    handleHome = () => {
+        return (
+            <div>
+                Under Construction...
+                </div>
+        )
+    }
+
+ 
+
+
+
+    render() {
+        return (
+            <div>
+                <Switch>
+                    <Route exact path='/NeighborGood' render={this.handleHome} />
+                    <Route exact path='/NeighborGood/map' render={this.handleMap} />
+                </Switch>
+            </div>
+        )
+    }
+}
+
+
+export default NeighborGood
