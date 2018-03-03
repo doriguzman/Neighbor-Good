17352da59b61119da78eb3628e48a283a449c953
janhairis <janhairisfermin@ac.c4q.nyc>
2018-03-03 15:55:35 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -5,14 +5,16 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className='App'>
+      <nav id='nav bar'>Home  Map</nav>
+      <div id='mapContainer'>
+        <div className='map'></div>
+        <div className='key'></div>
+      </div>
+      <div id='home'>
+        <div className='title'></div>
+        <div className='userstroies'></div>
+      </div>
       </div>
     );
   }
