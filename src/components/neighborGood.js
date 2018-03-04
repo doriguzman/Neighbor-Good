f5f2cf689a2ad8b0e1d0eb41d67df0525e5181a9
janhairis <janhairisfermin@ac.c4q.nyc>
2018-03-04 11:40:33 -0500

    sdfghjk

--- src/components/neighborGood.js
+++ src/components/neighborGood.js
@@ -32,7 +32,7 @@ class NeighborGood extends React.Component {
         const { SelectedComplaint } = this.state
         return (
             <div id='titleCon'>
-                <h1 id="title">Neighbor-Good</h1>
+                <h1 id="title">NeighborGood</h1>
                 <Map onComplaintClick={this.handleComplaint} />
                 {SelectedComplaint ? ComplaintInfo(SelectedComplaint) : <strong id='compDes'> Choose a complaint </strong>}
                 <br />
