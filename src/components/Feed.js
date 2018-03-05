81fdfa502ea8b4c48fd4de9e170134383b359043
vmack <vanessacmack@gmail.com>
2018-03-04 21:47:21 -0500

    added background color, changes to image and font

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -67,8 +67,7 @@ class Feed extends React.Component {
             <div id='totalFeed'>
                 <div id='feedImage' > </div>
             <div id="feed">
-                
-                <h1>Most Recent Complaints</h1>
+                <h1 id='feedTitle'> Most Recent Complaints</h1>
                 <a href={`https://streeteasy.com/for-sale/nyc/status:open%7Czip:${userInput}?refined_search=true`} target="_blank">Find apartments On StreetEasy!</a>
                 <br />
                 Enter a Zip Code: {" "}
