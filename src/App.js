307b7d9e4ba793eedd8e98ebe417ff620bad2278
newton-brooks <newtonbrooks.ac.c4q.nyc>
2018-03-04 19:05:06 -0500

    fixed nav bar

--- src/App.js
+++ src/App.js
@@ -7,12 +7,13 @@ import Home from './components/Home/Home.js'
 class App extends Component {
   render() {
     return (
-      <div>
+      <div id='thewholething'>
      
 <div id='navHolder'>
-
+<div>
 <img id='logo' src='https://thumbs.dreamstime.com/b/neighborhood-icon-vector-suburban-silhouette-design-element-43699957.jpg' height='90px'/>
 <h2 id='logoName'> NeighborGood </h2>
+</div>
       <nav>
           <li class='navlink'> <Link class='navlink-a' to='/'>Home</Link>{" "} </li>
           <li class='navlink'> <Link class='navlink-a' to='/NeighborGood/Map'>Map</Link>{" "} </li>
